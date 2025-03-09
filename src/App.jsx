0601479946cdc5b737005dbf24438d7fab7eeff8
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-03-09 17:08:53 +0200

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,32 @@
 import { Component } from "react";
-import Class from "./components/Class";
-import Clock from "./components/Clock";
-import Counter from "./components/Counter";
-import EventList from "./components/eventList/EventList";
-import { events } from "./recipies";
+import LoginForm from "./components/LoginForm";
 
-// extend - ключове слово яке говорить про наслідування
 class App extends Component {
   state = {
-    users: [1, 2, 3, 4, 5, 6, 7, 8],
-    events,
-    time: new Date().toLocaleTimeString(),
+    users: [{ name: "asd" }, { name: "qwe" }],
   };
 
   constructor() {
     super();
-    this.updateButtonTime = this.updateButtonTime.bind(this);
+    this.addNewUser = this.addNewUser.bind(this);
   }
 
-  updateButtonTime(e) {
-    console.log(e);
-    console.log(this);
-    this.setState({ time: new Date().toLocaleTimeString() });
-  }
-
-  printSomething() {
-    return "Привіт";
+  addNewUser(user) {
+    this.setState((state) => {
+      return state.users.push(user);
+    });
   }
 
   render() {
-    const users = this.state.users;
+    const { addNewUser } = this;
+
+    const { users } = this.state;
     return (
       <>
-        <div>
-          {users.map((user) => {
-            return <h1>{user}</h1>;
-          })}
-        </div>
-        <EventList events={this.state.events} method={this.printSomething} />
-        <Class method={this.printSomething} />
-        <Clock time={this.state.time} updateFunc={this.updateButtonTime} />
-
-        <button onClick={this.updateButtonTime}>
-          {this.state.time} "click to check time"
-        </button>
-
-        <Counter step={10} />
+        <LoginForm submitFunc={addNewUser} />
+        {users.map((user) => {
+          return <h1 key={user.name}>{user.name}</h1>;
+        })}
       </>
     );
   }
--- src/components/Class.jsx
+++
@@ -1,9 +0,0 @@
-import { Component } from "react";
-
-class Class extends Component {
-  render() {
-    return <h1>{this.props.method()}</h1>;
-  }
-}
-
-export default Class;
