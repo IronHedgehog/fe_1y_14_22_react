ab386e24a56a82206b8f304a3d49218bd403bbea
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-04-06 17:15:56 +0300

    practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,33 @@
 import { Component } from "react";
-import Filter from "./components/Filter";
-import Form from "./components/Form";
-import TodoList from "./components/TodoList";
+import Modal from "./components/modal/Modal";
 
 class App extends Component {
   state = {
-    todos: [
-      { id: 1, text: "do something", completed: true },
-      { id: 2, text: "do something2", completed: false },
-      { id: 3, text: "do something3", completed: true },
-      { id: 4, text: "do something4", completed: false },
-      { id: 5, text: "do something5", completed: false },
-    ],
-    filter: "",
-    error: false,
+    isModalOpen: false,
   };
+
   constructor() {
     super();
-    this.addTodo = this.addTodo.bind(this);
-    this.addFilter = this.addFilter.bind(this);
-    this.deleteTodo = this.deleteTodo.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
   }
 
-  componentDidMount() {
-    // http req, event, timer
-    const storageData = localStorage.getItem("todos");
-    console.log(storageData);
-    const parsedStorageData = JSON.parse(storageData);
-    console.log(parsedStorageData);
-
-    if (parsedStorageData) {
-      this.setState({
-        todos: parsedStorageData,
-      });
-    }
+  openModal() {
+    this.setState({ isModalOpen: true });
   }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    if (nextState.todos === this.state.todos) {
-      return false;
-    }
-    return true;
+  closeModal() {
+    this.setState({ isModalOpen: false });
   }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.todos !== prevState.todos) {
-      localStorage.setItem("todos", JSON.stringify(this.state.todos));
-    }
-  }
-
-  componentDidCatch() {
-    // слугує для обробки помилок та їх опрацювання
-    this.setState({ error: true });
-  }
-
-  addFilter(e) {
-    const { value } = e.target;
-    this.setState({ filter: value });
-  }
-
-  addTodo(newTodo) {
-    this.setState((prev) => ({
-      todos: [...prev.todos, newTodo],
-    }));
-  }
-
-  filterByText() {
-    const { todos, filter } = this.state;
-    console.log(
-      todos.filter((todo) =>
-        todo.text.toLowerCase().includes(filter.toLowerCase())
-      )
-    );
-  }
-
-  deleteTodo(id) {
-    this.setState((prevState) => ({
-      todos: prevState.todos.filter((todo) => todo.id !== id),
-    }));
-  }
-
   render() {
-    const filterElements = this.filterByText();
-    console.log(filterElements);
     return (
-      <div>
-        {this.state.error ? (
-          <h1>Сайт впав, приходьте пізніше</h1>
-        ) : (
-          <div>
-            <TodoList todos={this.state.todos} delete={this.deleteTodo} />
-            <Form addTodo={this.addTodo} />
-            <Filter
-              filterValue={this.state.filter}
-              addFilter={this.addFilter}
-            />
-          </div>
+      <>
+        <h1>Hello</h1>
+        <button onClick={this.openModal}>Відкриття модалки</button>
+        {this.state.isModalOpen && (
+          <Modal isOpen={this.state.isModalOpen} onClose={this.closeModal} />
         )}
-      </div>
+        ;
+      </>
     );
   }
 }
