649d3529c5323c1eef582caff161915592056ce0
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-05-04 15:03:53 +0300

    hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
   // state = {
-  //   news: [],
+  //   counter: 0,
   // };
+  // 10 - це значення за замовчуванням
   const [counter, setCounter] = useState(10);
+
+  // useEffect(
+  //   () => {
+  //     // componentDidMount
+
+  //     return () => {
+  //       // componentWillUnmount
+  //     };
+  //   }, //componentDidUpdate
+
+  //   []
+  // );
+  // componentDidMount
+  useEffect(() => {
+    document.title = `Ти клікнув ${counter} разів`;
+    console.log("abc");
+  }, []);
+
+  //componentDidUpdate
+  useEffect(() => {
+    document.title = `Ти клікнув ${counter} разів`;
+    console.log("abc");
+  }, [counter]);
   // const [] = useState([]);
   // const [first, setfirst] = useState(second);
-
+  // componentWillUnmount
+  useEffect(() => {
+    return () => {
+      console.log("Тут ви можете співпрацювата з елементом при його видаленні");
+    };
+  }, []);
   return (
     <div>
       {counter}
