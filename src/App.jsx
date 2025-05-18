dfd179b19bca8e061aa4603fa30233aceeb41336
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-05-18 14:44:49 +0300

    grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,18 @@
 import { useEffect, useRef, useState } from "react";
 
 const App = () => {
-  // const first = useRef(second);
-  // const first = useContext(second);
+  const [counter, setCounter] = useState(0);
+  const timerRef = useRef(null);
 
-  const refBtn = useRef();
-
-  // state = {
-  //   counter: 0,
-  // };
-  // 10 - це значення за замовчуванням
-  const [counter, setCounter] = useState(10);
-
-  // useEffect(
-  //   () => {
-  //     // componentDidMount
-
-  //     return () => {
-  //       // componentWillUnmount
-  //     };
-  //   }, //componentDidUpdate
-
-  //   []
-  // );
-  // componentDidMount
-  // useEffect(() => {
-  //   document.title = `Ти клікнув ${counter} разів`;
-  //   console.log("abc");
-  // }, []);
-
-  //componentDidUpdate
   useEffect(() => {
-    document.title = `Ти клікнув ${counter} разів`;
-    console.log(refBtn.current);
-  }, [counter]);
+    timerRef.current = setInterval(() => {
+      setCounter((count) => count + 1);
+    }, 1000);
 
-  const clickOnButton = () => {
-    console.log(refBtn);
-    console.log(refBtn.current);
-  };
+    return () => clearInterval(timerRef.current);
+  }, []);
 
-  // const [] = useState([]);
-  // const [first, setfirst] = useState(second);
-  // componentWillUnmount
-  // useEffect(() => {
-  //   return () => {
-  //     console.log("Тут ви можете співпрацювата з елементом при його видаленні");
-  //   };
-  // }, []);
-
-  console.log(refBtn.current);
-  return (
-    <div>
-      {counter}
-      <button
-        type="button"
-        ref={refBtn}
-        onClick={(e) => {
-          clickOnButton();
-          setCounter(counter + 1);
-        }}
-      >
-        +
-      </button>
-      <button
-        type="button"
-        onClick={(e) => {
-          setCounter(counter - 1);
-        }}
-      >
-        -
-      </button>
-    </div>
-  );
+  return <div>Час: {counter}</div>;
 };
 
 export default App;
